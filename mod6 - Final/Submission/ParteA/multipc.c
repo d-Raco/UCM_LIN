8ea608fc1496a0c2464a270cf2e393e9aa1475cf
Rymond3 <36478308+Rymond3@users.noreply.github.com>
2022-03-08 13:03:57 +0100

    Update multipc.c

--- mod6 - Final/Submission/ParteA/multipc.c
+++ mod6 - Final/Submission/ParteA/multipc.c
@@ -12,7 +12,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Multi producer-consumer Kernel Module - Arquitectura Interna de Linux y Android UCM");
-MODULE_AUTHOR("Ramón Costales de Ledesma");
+MODULE_AUTHOR("Rymond3");
 
 #define MAX_CHARS_KBUF	20
 #define MAX_CHARS_ADMIN 20
