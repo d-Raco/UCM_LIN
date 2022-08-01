5915ee6faa0a37879c668637f01833631290d03f
dRaco <36478308+d-Raco@users.noreply.github.com>
2022-08-01 12:21:37 +0200

    Update fifoproc.c

--- mod4 - FIFO pipes and syncronization/Submission/ParteB/fifoproc.c
+++ mod4 - FIFO pipes and syncronization/Submission/ParteB/fifoproc.c
@@ -8,7 +8,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Implementation of a FIFO pipe via /proc entry - Arquitectura Interna de Linux y Android UCM");
-MODULE_AUTHOR("Rymond3, joseignaciodg");
+MODULE_AUTHOR("d-Raco, joseignaciodg");
 
 #define MAX_KBUF	64
 #define MAX_ITEMS_CBUF 64
